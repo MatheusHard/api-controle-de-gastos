656157e350ecbe42a1b268decaab370d8825b50a
MatheusHard <matheus@novetech.com.br>
2026-06-25 16:31:31 -0300

    [CONFIG] buildar projeto

--- src/main/java/com/infotrapichao/api_controle_de_gastos/ApiControleDeGastosApplication.java
+++ src/main/java/com/infotrapichao/api_controle_de_gastos/ApiControleDeGastosApplication.java
@@ -2,11 +2,13 @@ package com.infotrapichao.api_controle_de_gastos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ApiControleDeGastosApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(ApiControleDeGastosApplication.class, args);
 	}
 
--- src/main/java/com/infotrapichao/api_controle_de_gastos/src/distributed/interfaces/configuration/jwt/WebSecurityConfig.java
+++ src/main/java/com/infotrapichao/api_controle_de_gastos/src/distributed/interfaces/configuration/jwt/WebSecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
@@ -19,7 +19,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
     @Bean
     public BCryptPasswordEncoder encoder(){
